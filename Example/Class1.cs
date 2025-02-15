ca479bb0e96a767ccce262950a179c99be8372bc
Viktoriia Diachynska <vdiachynska@kse.org.ua>
2025-02-15 21:01:28 +0200

    Add project files.

---
+++ Example/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace Lecture5;
+public class Employee
+    {
+        
+        public virtual void DisplayInfo() {
+            Console.WriteLine($"{this.GetType().Name}: general information");
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"{this.GetType().Name}: specialized information");
+        }
+    }
+class Developer : Employee
+{
+
+}
+
---
+++ Example/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Lecture5
+{
+    interface IMovable
+    {
+        //int Position { get; }
+
+        void Move(int distance);
+    }
+
+    public class Robot : IMovable
+    {
+        public int Position
+        {
+            get;
+            private set;
+        } = 0;
+
+        public void Move(int distance)
+        {
+            if (distance < 0)
+                throw new ArgumentException("Distance can not be <0");
+            this.Position += distance;
+            Console.WriteLine($"Robot moved {distance} points. Current position: {this.Position}");
+        }
+        public Robot() { }
+    }
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            IMovable robot = new Robot();
+            robot.Move(10);
+            robot.Move(20);
+        }
+    }
+    
+}
+
+
+
