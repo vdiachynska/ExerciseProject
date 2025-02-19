c5503fc0776f4b009ca4294fc35ed462bf15a460
Viktoriia Diachynska <vdiachynska@kse.org.ua>
2025-02-19 19:04:38 +0200

    Framework for Practice6

---
+++ Example/TicketDesk.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public class TicketDesk : ITicketDesk
+    {
+        private readonly List<ITicket> _ticketList;
+        public List<ITicket> Tickets => _ticketList; //for the testing   
+
+        public TicketDesk()
+        {
+            _ticketList = new List<ITicket>();
+        }
+
+        public void AddTicket(ITicket ticket)
+        {
+            _ticketList.Add(ticket);
+        }
+
+        public ITicket GetTicket(int id)
+        {
+            return _ticketList[id];
+        }
+    }
+}
